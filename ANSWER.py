63515fbf22c3521ea39b7ea7c41b3055ac7c580d
KsushaSeliv <KsenijaSeliv@yandex.ru>
2018-01-11 00:02:09 +0300

    Update ANSWER.py

--- ANSWER.py
+++ ANSWER.py
@@ -1,10 +1,16 @@
 import itertools
-lst=[1,9,-20,-8,7,14,0,20,20,-20]
-a=list(itertools.combinations(lst,3))
-print(a)
-mul1=0
-for comb in a:
- mul=comb[0]*comb[1]*comb[2]
- if mul1 > mul:
-   mul1=mul
-print(mul1)
+def proizv(lst):
+    if len(lst) < 3:
+        raise Exception('Less than 3 items!')
+    a=list(itertools.combinations(lst,3))
+    print(a)
+    mul1=0
+    for comb in a:
+     mul=comb[0]*comb[1]*comb[2]
+    if mul1 > mul:
+     mul1=mul
+    return mul1
+
+if __name__ == '__main__':
+    lst=[1,9,-20,-8,7,14,0,20,20,-20]
+    print(proizv(lst))
